2da36ee4c0c5a0b95a74149ea4baf18340fbe521
Vinicius Hespanha <mail@vhespanha.dev>
2026-01-20 13:35:13 -0300

    add encoding/decoding options

---
+++ gob.go
@@ -0,0 +1,46 @@
+package ngrams
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+func (t *Table[T]) GobEncode() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	if err := enc.Encode(t.n); err != nil {
+		return nil, err
+	}
+	if err := enc.Encode(t.freqs); err != nil {
+		return nil, err
+	}
+	if err := enc.Encode(t.total); err != nil {
+		return nil, err
+	}
+	if err := enc.Encode(t.alphabet); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (t *Table[T]) GobDecode(data []byte) error {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+
+	if err := dec.Decode(&t.n); err != nil {
+		return err
+	}
+	if err := dec.Decode(&t.freqs); err != nil {
+		return err
+	}
+	if err := dec.Decode(&t.total); err != nil {
+		return err
+	}
+	if err := dec.Decode(&t.alphabet); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ json.go
@@ -0,0 +1,29 @@
+package ngrams
+
+import "encoding/json"
+
+type auxTable[T freq] struct {
+	N        int       `json:"n"`
+	Freqs    []T       `json:"freqs"`
+	Total    float64   `json:"total"`
+	Alphabet *Alphabet `json:"alphabet"`
+}
+
+func (t *Table[T]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(auxTable[T]{t.n, t.freqs, t.total, t.alphabet})
+}
+
+func (t *Table[T]) UnmarshalJSON(data []byte) error {
+	aux := auxTable[T]{}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	t.n = aux.N
+	t.freqs = aux.Freqs
+	t.total = aux.Total
+	t.alphabet = aux.Alphabet
+
+	return nil
+}
